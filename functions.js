5350290edcb3ffdfd395defa736e213208e1650a
Simona <simona.glodean90@gmail.com>
2021-01-11 16:11:40 +0200

    add and delete team member

--- functions.js
+++ functions.js
@@ -1,3 +1,24 @@
+const API = {
+    CREATE: {
+        URL: "create.json",
+        METHOD: "GET"
+    },
+    READ: {
+        URL: "teams.json",
+        METHOD: "GET"
+    },
+    UPDATE: {
+        URL: "",
+        METHOD: "GET"
+    },
+    DELETE: {
+        URL: "delete.json",
+        METHOD: "GET"
+    }
+};
+
+
+
 function insertPersons (persons) {
     const tbody = document.querySelector('#list tbody');
     tbody.innerHTML = getPersonsHtml(persons);
@@ -12,25 +33,33 @@ function getPersonHtml (person) {
     return ` <tr>
         <td>${person.firstName}</td>
         <td>${person.lastName}</td>
-        <td><a target="_blank" href="https://github.com/${person.gitHub}">gitHub</a></td>
+        <td><a target="_blank" href="https://github.com/${person.gitHub}">GitHub</a></td>
+        <td>
+            <a href="${API.DELETE.URL}?id=${person.id}">&#10006;</a>
+        </td>
     </tr>`; 
 }
 
 let allPersons = [];
 
-fetch('teams.json')
+function loadList() {
+    fetch(API.READ.URL)
     .then(res => res.json())
     .then(data => { 
         allPersons = data; 
         insertPersons(data);
     });
+}
+
+loadList();
 
 function searchPersons(text) {
     text = text.toLowerCase();
     console.warn("search", text);
     return allPersons.filter(person => {
         console.info(person.firstName);
-        return person.firstName.toLowerCase().indexOf(text) > -1;
+        return person.firstName.toLowerCase().indexOf(text) > -1 ||
+             person.lastName.toLowerCase().indexOf(text) > -1 ;
     });
 }
 
@@ -41,4 +70,34 @@ search.addEventListener("input", e => {
     const filtrate = searchPersons(text);
 
     insertPersons(filtrate);
-})
+});
+
+function saveTeamMember() {
+    const firstName = document.querySelector("input[name=firstName]").value;
+    const lastName = document.querySelector("input[name=lastName]").value;
+    const gitHub = document.querySelector("input[name=gitHub]").value;
+
+    const person = {
+        firstName, lastName, gitHub
+    };
+    console.info('saving', person, JSON.stringify(person));
+
+    fetch(API.CREATE.URL, {
+        method: API.CREATE.METHOD,
+        body: API.CREATE.METHOD === "GET" ? null : JSON.stringify(person)
+    })
+    .then(res => res.json())
+    .then(r => {
+        console.warn(r)
+        if(r.success) {
+            alert('saving data, please wait');
+                console.info('refresh list');
+                loadList(); 
+        }
+    });
+}
+
+const saveBtn = document.querySelector("#list tfoot button");
+saveBtn.addEventListener("click", () => {
+   saveTeamMember();
+});
